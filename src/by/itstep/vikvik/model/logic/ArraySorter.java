d57d4251e0412066c7a2e729d7b5ba1d5e142c55
vikvik <ivanvikvik@gmail.com>
2022-10-04 10:53:18 +0300

    Added selected sorting algorithm and updated bubble sorting algorithm

--- src/by/itstep/vikvik/model/logic/ArraySorter.java
+++ src/by/itstep/vikvik/model/logic/ArraySorter.java
@@ -1,7 +1,32 @@
 package by.itstep.vikvik.model.logic;
 
 public class ArraySorter {
-    public static void bubbleSortAsc(int[] array) {
+    public static void selectedSortAsc(int[] array) {
+
+        if (array == null) {
+            throw new RuntimeException();
+        }
+
+        for (int j = 0; j < array.length - 1; j++) {
+
+            int indexMinElement = j;
+
+            for (int i = j + 1; i < array.length; i++) {
+                if (array[indexMinElement] > array[i]) {
+                    indexMinElement = i;
+                }
+            }
+
+            int t = array[j];
+            array[j] = array[indexMinElement];
+            array[indexMinElement] = t;
+
+        }
+
+    }
+
+
+        public static void bubbleSortAsc(int[] array) {
 
         int count = 0;  // debug
 
@@ -11,14 +36,35 @@ public class ArraySorter {
         }
 
         for (int j = 0; j < array.length - 1; j++) {
-            for (int i = 0; i < array.length - 1; i++) {
+            for (int i = 0; i < array.length - 1 - j; i++) {
                 count++;    // debug
                 if (array[i] > array[i + 1]) {
                     int t = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = t;
                 }
+            }
+        }
+        System.out.println("count = " + count);   // debug
+    }
 
+    public static void bubbleSortDesc(int[] array) {
+
+        int count = 0;  // debug
+
+        if (array == null) {
+//            return;
+            throw new RuntimeException();
+        }
+
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                count++;    // debug
+                if (array[i] < array[i + 1]) {
+                    int t = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = t;
+                }
             }
         }
         System.out.println("count = " + count);   // debug
--- test/by/itstep/vikvik/model/logic/ArraySorterTest.java
+++ test/by/itstep/vikvik/model/logic/ArraySorterTest.java
@@ -29,4 +29,51 @@ public class ArraySorterTest {
         ArraySorter.bubbleSortAsc(new int[0]);
     }
 
+    @Test
+    public void testBubbleSortDesc() {
+        int[] array = {7, 5, 6, 4, 5, 3, 4, 2, 3, 1};
+        int[] expected = {7, 6, 5, 5, 4, 4, 3, 3, 2, 1};
+
+        ArraySorter.bubbleSortDesc(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != expected[i]) {
+                fail();
+            }
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testBubbleSortDescWithNull() {
+        ArraySorter.bubbleSortDesc(null);
+    }
+
+    @Test
+    public void testBubbleSortDescWithEmptyObject() {
+        ArraySorter.bubbleSortDesc(new int[0]);
+    }
+
+    @Test
+    public void testSelectedSortAsc() {
+        int[] array = {7, 5, 6, 4, 5, 3, 4, 2, 3, 1};
+        int[] expected = {1, 2, 3, 3, 4, 4, 5, 5, 6, 7};
+
+        ArraySorter.selectedSortAsc(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != expected[i]) {
+                fail();
+            }
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSelectedSortAscWithNull() {
+        ArraySorter.selectedSortAsc(null);
+    }
+
+    @Test
+    public void testSelectedSortAscWithEmptyObject() {
+        ArraySorter.selectedSortAsc(new int[0]);
+    }
 }
